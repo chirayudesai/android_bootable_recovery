ca0f156b9ed9150498f903483387eb6c7be342fb
Chirayu Desai <chirayudesai1@gmail.com>
2012-07-29 19:43:52 +0530

    Android.mk: add CD to RECOVERY_NAME
    just so that i can know i am using my recovery
    
    Change-Id: I1d610020331d6cc712ceb7476367417d88e6e3c7

--- Android.mk
+++ Android.mk
@@ -32,7 +32,7 @@ ifdef I_AM_KOUSH
 RECOVERY_NAME := ClockworkMod Recovery
 LOCAL_CFLAGS += -DI_AM_KOUSH
 else
-RECOVERY_NAME := CWM-based Recovery
+RECOVERY_NAME := CWM-based Recovery - CD
 endif
 
 RECOVERY_VERSION := $(RECOVERY_NAME) v6.0.1.0
